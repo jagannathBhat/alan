61f3216e176fd28d362141f7eadcad18c97602a0
Jagannath Bhat <jagannathbhatjb@gmail.com>
2022-06-13 09:08:02 +0530

    Added permission button

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Popup = () => (
   <div className="popup">
-    <h1>alan</h1>
+    <h1 className="popup-title">alan</h1>
+    <button className="button" id="permission-button">
+      allow recording
+    </button>
+    <p className="subtitle">
+      the extension requires recording permission to record audio while taking
+      the test
+    </p>
   </div>
 );
 
