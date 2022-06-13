574e4934d25d84a9d1aa17fca5bc1792392b787b
Jagannath Bhat <jagannathbhatjb@gmail.com>
2022-06-13 09:27:30 +0530

    Added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import "./App.css";
-import routes from "./routes";
+import React, { useEffect, useRef, useState } from "react";
 
-const App = () => <>{routes[window.location.hash] ?? routes.default}</>;
+import Routes from "./Routes";
+import "./App.css";
+
+const App = () => {
+  const [hash, setHash] = useState(window.location.hash);
+  const hashChangeListener = useRef(null);
+
+  useEffect(() => {
+    hashChangeListener.current = window.addEventListener("hashchange", () =>
+      setHash(window.location.hash),
+    );
+    return () =>
+      window.removeEventListener("hashchange", hashChangeListener.current);
+  }, []);
+
+  return <Routes hash={hash} />;
+};
 
 export default App;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,8 +4,11 @@ import Options from "./components/Options";
 import Popup from "./components/Popup";
 
 const routes = {
-  default: <Popup />,
+  default: <>404</>,
+  "": <Popup />,
   "#options": <Options />,
 };
 
-export default routes;
+const Routes = ({ hash }) => routes[hash] ?? routes.default;
+
+export default Routes;
