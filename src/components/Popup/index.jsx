4f260deae73d1af191031d31d2504d64d2707413
Jagannath Bhat <jagannathbhatjb@gmail.com>
2022-06-16 09:36:16 +0530

    Added link to Options page

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Popup = () => (
   <div className="popup">
     <h1 className="popup-title">alan</h1>
-    <button className="button" id="permission-button">
-      allow recording
-    </button>
+    <a href="#options" target="_blank" rel="noopener noreferrer">
+      <button className="button" style={{ marginTop: 12 }}>
+        allow recording
+      </button>
+    </a>
     <p className="subtitle">
       the extension requires recording permission to record audio while taking
       the test
