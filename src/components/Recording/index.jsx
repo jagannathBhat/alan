70b2dba28437937fe675e5a37356765670d145b9
Jagannath Bhat <jagannathbhatjb@gmail.com>
2022-06-20 08:28:07 +0530

    Added controls to Recording page

--- src/components/Recording/index.jsx
+++ src/components/Recording/index.jsx
@@ -4,6 +4,17 @@ const Recording = () => {
   return (
     <div className="page">
       <h1>Recording</h1>
+      <button className="button">Share Screen</button>
+      <p className="subtitle">
+        Choose a screen to record by clicking the above button
+      </p>
+      <div className="controls">
+        <button className="button">Resume/Pause</button>
+        <button className="button">Stop</button>
+        <button className="button">Cancel</button>
+      </div>
+      <p>Something went wrong with the recorder</p>
+      <button className="button">Try Again</button>
     </div>
   );
 };
