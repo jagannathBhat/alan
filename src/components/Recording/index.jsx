5219121ed268ff6ffcc0f9cbe9ba20ba72fe1f1f
Jagannath Bhat <jagannathbhatjb@gmail.com>
2022-06-21 09:02:44 +0530

    Moved components to frames

--- src/components/Recording/index.jsx
+++ src/components/Recording/index.jsx
@@ -14,27 +14,26 @@ const Recording = () => {
     a.click();
   };
 
-  return (
-    <div className="page">
-      <h1>Recording</h1>
-      <button
-        className="button"
-        onClick={() => {
-          console.log("clicked");
-          recorder.enable();
-        }}
-      >
-        Share Screen
-      </button>
-      <p className="subtitle">
-        Choose a screen to record by clicking the above button
-      </p>
+  const frames = {
+    prompt: (
+      <>
+        <button className="button" onClick={() => recorder.enable()}>
+          Share Screen
+        </button>
+        <p className="subtitle">
+          Choose a screen to record by clicking the above button
+        </p>
+      </>
+    ),
+    ready: (
       <button className="button" onClick={() => recorder.start()}>
         Start Recording
       </button>
+    ),
+    active: (
       <div className="controls">
-        <button className="button" onClick={() => recorder.pause()}>
-          Resume/Pause
+        <button className="button" onClick={() => recorder.resumeOrPause()}>
+          {recorder.isPaused ? "Resume" : "Pause"}
         </button>
         <button className="button" onClick={stopRecording}>
           Stop
@@ -43,10 +42,21 @@ const Recording = () => {
           Cancel
         </button>
       </div>
-      <p>Something went wrong with the recorder</p>
-      <button className="button" onClick={() => recorder.enable()}>
-        Try Again
-      </button>
+    ),
+    error: (
+      <>
+        <p>Something went wrong with the recorder</p>
+        <button className="button" onClick={() => recorder.enable()}>
+          Try Again
+        </button>
+      </>
+    ),
+  };
+
+  return (
+    <div className="page">
+      <h1>Recording</h1>
+      {frames[recorder.status] ?? frames.prompt}
     </div>
   );
 };
